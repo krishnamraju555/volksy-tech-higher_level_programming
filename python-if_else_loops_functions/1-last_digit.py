ec63870aeb154c4ecce07f653fec432b0296b201
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-01-01 22:18:56 -0800

    one

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+a = number % 10
+if str(number)[0] == "-":
+    a = a-10
+if a > 5:
+    print("Last digit of", number, "is", a, "and is greater than 5")
+elif a == 0:
+    print("Last digit of", number, "is", a, "and is 0")
+elif a < 6 and a != 0:
+    print("Last digit of", number, "is", a, "and is less than 6 and not 0")
