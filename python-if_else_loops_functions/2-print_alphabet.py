031f8cd37bb39231baae7b0b1792f59834138060
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-01-01 23:27:18 -0800

    Two

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-    print("{:c}".format(i), end="")
+for alpha_letters in range(ord('a'), ord('z')+1):
+    print("{:c}".format(alpha_letters), end="")
