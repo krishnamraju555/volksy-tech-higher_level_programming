340bb459ffc7ecfb89af550dfd9d0f46c62b0b4e
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-01-08 19:38:16 -0800

    Seven

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    """Replace all occurrences of an element by another in a new list."""
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return (new_list)
+def print_sorted_dictionary(a_dictionary):
+    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
