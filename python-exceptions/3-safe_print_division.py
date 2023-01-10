4a4a26f5bd04e1d5335c143a355451deb2d845d5
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-01-09 19:53:26 -0800

    Three

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3                                                              def safe_print_division(a, b):                                                      try:                                  
+        result = a / b
+        return result                                                               except:                                                                             result = None                              
+        return result                                                           
+    finally:                              
+        print("Inside result: {}".format(result))
