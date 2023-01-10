5fa97fc9c98d2cab3f4a1bd2663c7eff2e02e2ed
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-01-09 22:08:57 -0800

    7

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,4 +6,4 @@ def safe_print_integer_err(value):
         return True
     except Exception as err:
         print("Exception: {}".format(err)), file=stderr
-        return false
+        return False
