294272baddbb303fa2b1b6cdd4930e23cff2feaf
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-01-11 03:43:13 -0800

    one

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Square:
+    """square is size"""
+    def __init__(self, size=0):
+        """constructor"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise valueError("size must be >= 0")
+        self.__size = size
