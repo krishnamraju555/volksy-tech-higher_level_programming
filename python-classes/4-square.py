a315f9b8eb2bfa59f224f7e28448287409a00881
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-01-11 20:00:06 -0800

    Four

---
+++ python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This is a class"""
+
+
+class Square:
+    """ This is bad """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
