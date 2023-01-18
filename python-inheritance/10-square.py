b0d806e3a65004c9ee2cfaf8b7a7fafa37f7740a
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-01-17 21:48:13 -0800

    10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    class Square that inherits from Rectangle (9-rectangle.py):
+    Instantiation with size: def __init__(self, size)::
+        size must be private. No getter or setter
+        size must be a positive integer, validated by
+        integer_validator
+    the area() method must be implemented
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class square inherits from rectangle class """
+
+    def __init__(self, size):
+        """ initializes square """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
