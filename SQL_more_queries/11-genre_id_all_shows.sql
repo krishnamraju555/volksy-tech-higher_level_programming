825b8a32592f189ab200eaae5fad22f8c7d68502
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-02-02 02:26:59 -0800

    11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Displays NULL for shows without genres.
+SELECT s.'title', g.'genre_id'
+  FROM 'tv_shows' AS s
+       LEFT JOIN 'tv_show_genres' AS g
+       ON s.'id' = g.'show_id'
+ ORDER BY s.'title', g.'genre_id';
