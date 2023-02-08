a892ecb694c4f2134a10cfe0a4282e8b8314f36e
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-02-08 02:48:36 -0800

    3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
+"""
+takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument,
+safe from MySQL injections
+"""
 import MySQLdb
 from sys import argv
 
