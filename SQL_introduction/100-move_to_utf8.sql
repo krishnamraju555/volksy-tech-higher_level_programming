dd74da3e05a5f9b9335af6c75619eaf3cabca0c8
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-02-08 20:52:14 -0800

    100

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,5 @@
--- hbtn_0c_0 converted to UTF8
--- sets database
-USE hbtn_0c_0;
--- converts field to utf8mb4
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
--- converts table to utf8mb4
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- converts database to utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Go to UTF8
+-- convert db
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- covert table
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;:wq
