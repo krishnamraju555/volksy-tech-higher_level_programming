cc91560ed997e4885c05aca6c942270f9f6b75e8
krishnamraju555 <krishnamraju@brightcomgroup.com>
2023-02-08 20:16:40 -0800

    100

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("Holberton, " * (magic_string.n - 1) + "Holberton")
+    setattr(magic_string, "x", getattr(magic_string, "x", -1) + 1)
+    return "Holberton" + ", Holberton" * getattr(magic_string, "x", 0)
